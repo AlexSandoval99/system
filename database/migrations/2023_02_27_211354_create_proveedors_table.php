3f8b8ae25fb19f358e5f00507321884dfa906b0c
AlexSandoval99 <102924381+AlexSandoval99@users.noreply.github.com>
2023-08-05 23:08:57 -0400

    provider

---
+++ database/migrations/2023_02_27_211354_create_proveedors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProveedorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('ruc');
+            $table->string('address');
+            $table->string('phone');
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedors');
+    }
+}
